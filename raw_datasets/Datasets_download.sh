dc8e0dbb2aaa93349ec430d922b1f1b1ae36c06a
AlexisLaf <150516817+AlexisLaf@users.noreply.github.com>
2026-06-27 19:49:11 +0200

    fix if the issue was with the curl command

--- raw_datasets/Datasets_download.sh
+++ raw_datasets/Datasets_download.sh
@@ -22,20 +22,20 @@
 
 #The curl command that are not used are to limit the amount of download done each time when github performs the action.
 
-#curl -L "https://drive.usercontent.google.com/download?id=1GjWrIZq_mx8Ugpxrm-_yzzbXzMhZ3FAk" -o 'raw_datasets/bus/lyon_tcl/Agency.txt' --create-dirs
-#curl -L "https://drive.usercontent.google.com/download?id=1EXi_fUcYtKaUYJA5NIuoTMbljo9Hq2YH" -o 'raw_datasets/bus/lyon_tcl/Calendar dates.txt'
-curl -L "https://drive.usercontent.google.com/download?id=1y1JWkDTyFtSttHzhAsDBwhIqMWeYFHSY" -o 'raw_datasets/bus/lyon_tcl/trips.txt' --create-dirs
-#curl -L "https://drive.usercontent.google.com/download?id=1R-EA9Wr58JmG1to4r9OX8IP73Fi9BlA7" -o 'raw_datasets/bus/lyon_tcl/calendar.txt'
-#curl -L "https://drive.usercontent.google.com/download?id=1j0L2ZQIysFtpHXmDMVNxKvl5ksljOz_C" -o 'raw_datasets/bus/lyon_tcl/fare_attributes.txt'
-#curl -L "https://drive.usercontent.google.com/download?id=1X8zInfwPeClbLZO_AUmc6cIe-0vZuTzv" -o 'raw_datasets/bus/lyon_tcl/fare_rules.txt'
-#curl -L "https://drive.usercontent.google.com/download?id=1QGA8kJyxtTpX1DT091M4z8KlZDfA8y5s" -o 'raw_datasets/bus/lyon_tcl/feed_info.txt'
-#curl -L "https://drive.usercontent.google.com/download?id=1X9FnWRx7knyiztP-GKDUa_FMifSUN-3k" -o 'raw_datasets/bus/lyon_tcl/frequencies.txt'
-#curl -L "https://drive.usercontent.google.com/download?id=105SVONlU-kcgqYUv50jlnYftTyzkS6vU" -o 'raw_datasets/bus/lyon_tcl/id_mappings.txt'
-#curl -L "https://drive.usercontent.google.com/download?id=1uFlgr0VIMATCc2n2K3p074WLCIpbFUDG" -o 'raw_datasets/bus/lyon_tcl/routes.txt'
-#curl -L "https://drive.usercontent.google.com/download?id=14ZFanoyzm6VtNT6y6B0M8F_9HVY1ryDA&confirm" -o 'raw_datasets/bus/lyon_tcl/shapes.txt' #(/!\ TOO BIG CANNOT DIRECTLY DOWNLOAD)
-curl -L "https://drive.usercontent.google.com/download?id=1MSZ9l3ur_IjNrx4iom7Af4dMLvhgbG7p&confirm" -o 'raw_datasets/bus/lyon_tcl/Stop_times.txt' #Too big to download normally so we add &confirm to bypass the confirmation screen
-curl -L "https://drive.usercontent.google.com/download?id=1SWU4ytmigCsoPYn2juqbQ_d29NKATu19" -o 'raw_datasets/bus/lyon_tcl/stops.txt'
-#curl -L "https://drive.usercontent.google.com/download?id=1Fy8Ip9c57B5M1SOwDNfe1l2EUmkd2Wwm" -o 'raw_datasets/bus/lyon_tcl/transfers.txt'
+#curl -o "raw_datasets/bus/lyon_tcl/Agency.txt" -L "https://drive.usercontent.google.com/download?id=1GjWrIZq_mx8Ugpxrm-_yzzbXzMhZ3FAk" --create-dirs
+#curl -o "raw_datasets/bus/lyon_tcl/Calendar dates.txt" -L "https://drive.usercontent.google.com/download?id=1EXi_fUcYtKaUYJA5NIuoTMbljo9Hq2YH" 
+curl -o "raw_datasets/bus/lyon_tcl/trips.txt" -L "https://drive.usercontent.google.com/download?id=1y1JWkDTyFtSttHzhAsDBwhIqMWeYFHSY" --create-dirs
+#curl -o "raw_datasets/bus/lyon_tcl/calendar.txt" -L "https://drive.usercontent.google.com/download?id=1R-EA9Wr58JmG1to4r9OX8IP73Fi9BlA7" 
+#curl -o "raw_datasets/bus/lyon_tcl/fare_attributes.txt" -L "https://drive.usercontent.google.com/download?id=1j0L2ZQIysFtpHXmDMVNxKvl5ksljOz_C" 
+#curl -o "raw_datasets/bus/lyon_tcl/fare_rules.txt" -L "https://drive.usercontent.google.com/download?id=1X8zInfwPeClbLZO_AUmc6cIe-0vZuTzv" 
+#curl -o "raw_datasets/bus/lyon_tcl/feed_info.txt" -L "https://drive.usercontent.google.com/download?id=1QGA8kJyxtTpX1DT091M4z8KlZDfA8y5s" 
+#curl -o "raw_datasets/bus/lyon_tcl/frequencies.txt" -L "https://drive.usercontent.google.com/download?id=1X9FnWRx7knyiztP-GKDUa_FMifSUN-3k" 
+#curl -o "raw_datasets/bus/lyon_tcl/id_mappings.txt" -L "https://drive.usercontent.google.com/download?id=105SVONlU-kcgqYUv50jlnYftTyzkS6vU" 
+#curl -o "raw_datasets/bus/lyon_tcl/routes.txt" -L "https://drive.usercontent.google.com/download?id=1uFlgr0VIMATCc2n2K3p074WLCIpbFUDG" 
+#curl -o "raw_datasets/bus/lyon_tcl/shapes.txt" -L "https://drive.usercontent.google.com/download?id=14ZFanoyzm6VtNT6y6B0M8F_9HVY1ryDA&confirm" #(/!\ TOO BIG CANNOT DIRECTLY DOWNLOAD)
+curl -o "raw_datasets/bus/lyon_tcl/Stop_times.txt" -L "https://drive.usercontent.google.com/download?id=1MSZ9l3ur_IjNrx4iom7Af4dMLvhgbG7p&confirm" #Too big to download normally so we add &confirm to bypass the confirmation screen
+curl -o "raw_datasets/bus/lyon_tcl/stops.txt" -L "https://drive.usercontent.google.com/download?id=1SWU4ytmigCsoPYn2juqbQ_d29NKATu19" 
+#curl -o "raw_datasets/bus/lyon_tcl/transfers.txt" -L "https://drive.usercontent.google.com/download?id=1Fy8Ip9c57B5M1SOwDNfe1l2EUmkd2Wwm" 
 
 
 #Metro :
@@ -45,10 +45,9 @@ curl -L "https://drive.usercontent.google.com/download?id=1SWU4ytmigCsoPYn2juqbQ
 
 #The curl command that are not used are to limit the amount of download done each time when github performs the action.
 
-curl -L "https://drive.usercontent.google.com/download?id=15gYhnrVg9B7p1QEf143FMG0-8Ieb2ieb" -o 'raw_datasets/metro/horaires_tcl.csv' --create-dirs
-#curl -L "https://drive.usercontent.google.com/download?id=1Q8iHKB9dEe3Gkj3tUOcZZRuRaTfoLtIJ" -o 'raw_datasets/metro/lignes-metro-funiculaire-reseau-transports-commun-lyonnais.csv'
-curl -L "https://drive.usercontent.google.com/download?id=17hCUhL46OkZ6Am4ykDPDuCzSqhw_r0y-" -o 'raw_datasets/metro/stations-metro-reseau-transports-commun-lyonnais.csv'
-
+curl -o "raw_datasets/metro/horaires_tcl.csv" -L "https://drive.usercontent.google.com/download?id=15gYhnrVg9B7p1QEf143FMG0-8Ieb2ieb" --create-dirs
+#curl -o "raw_datasets/metro/lignes-metro-funiculaire-reseau-transports-commun-lyonnais.csv" -L "https://drive.usercontent.google.com/download?id=1Q8iHKB9dEe3Gkj3tUOcZZRuRaTfoLtIJ" 
+curl 'raw_datasets/metro/stations-metro-reseau-transports-commun-lyonnais.csv' -L "https://drive.usercontent.google.com/download?id=17hCUhL46OkZ6Am4ykDPDuCzSqhw_r0y-" -o 
 
 
 
